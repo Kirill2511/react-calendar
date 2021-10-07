4b510be91a8cc30b3bf9452838485970a54d28da
Kirill Larionov <kirill.kirill2@yandex.ru>
2021-10-07 13:00:47 +0500

    init

---
+++ src/pages/Event.tsx
@@ -0,0 +1,41 @@
+import React, {useEffect, useState} from 'react';
+import {Button, Layout, Modal, Row} from "antd";
+import EventCalendar from "../components/Calendar";
+import EventForm from "../components/EventForm";
+import {useActions} from "../hooks/useActions";
+import {useTypedSelector} from "../hooks/useTypedSelector";
+import {IEvent} from "../models/IEvent";
+
+const Event = () => {
+    const [modalVisible, setModalVisible] = useState(false)
+    const {fetchGuests, createEvent, fetchEvent} = useActions()
+    const {guests, events} = useTypedSelector(state => state.event)
+    const {user} = useTypedSelector(state => state.auth)
+
+
+    useEffect(() => {
+        fetchGuests()
+        fetchEvent(user.username)
+    }, [])
+
+    const addNewEvent = (event: IEvent) => {
+        setModalVisible(false)
+        createEvent(event)
+    }
+
+    return (
+        <Layout>
+            <EventCalendar events={events}/>
+            <Row justify="center">
+                <Button onClick={() => setModalVisible(true)}>
+                    Добавить событие
+                </Button>
+            </Row>
+            <Modal title="Добавить событие" visible={modalVisible} footer={null} onCancel={() => setModalVisible(false)}>
+                <EventForm guests={guests} submit={addNewEvent} />
+            </Modal>
+        </Layout>
+    )
+}
+
+export default Event;
